4f1b53b9104eea020e0b2df92e7c5866c425363b
pritibandewar-mindbowser <priti.b@mindbowser.com>
2021-11-05 21:58:03 +0000

    Added logic for twing code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {chunkedArrayFunction} from './chunkedArray.js'
+
 import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <p>Welcome</p>
+      {console.log("ele", chunkedArrayFunction([1,2,3,4,5,6,7,8,9,10], 6))}
+      <p>{chunkedArrayFunction([1,2,3,4,5,6,7,8,9,10], 6)}</p>
     </div>
   );
 }
---
+++ src/validator.js
@@ -0,0 +1,13 @@
+export const Validator = (array, numberOfSubArray) => {
+  if(array.length <=0){
+    return "Please provide valid Array";
+  }
+  if (typeof numberOfSubArray !== "number" || numberOfSubArray === 0) {
+    return "Please provide valid subarray number greater than 0";
+  }
+  if (array.length < numberOfSubArray) {
+    return `This array cannot be divide in ${numberOfSubArray} subarrays`;
+  }
+
+  return "Success";
+};
