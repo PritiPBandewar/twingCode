f3caee2b73d48d992c30cbb149eaf6bfd0d8f331
pritibandewar-mindbowser <priti.b@mindbowser.com>
2021-11-06 23:48:39 +0000

    Readme file changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { groupArrayElements } from "./groupArrayElements.js";
 
 import "./App.css";
 
-const input = [1, 2, 3, 4, 5];
-const size = 3;
-const result = groupArrayElements(input, size);
+let input = [1, 2, 3, 4, 5];
+let size = 3;
+let result = groupArrayElements(input.slice(), size);
 
 function App() {
   return (
     <div className="App">
+      <pre>Input :{JSON.stringify(input)}</pre>
+      <pre>size :{JSON.stringify(size)} </pre>
       <pre>Output :{JSON.stringify(result)}</pre>
     </div>
   );
 }
-
 export default App;
