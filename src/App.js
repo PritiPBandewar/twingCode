39cc74e7d2214e721d58742b7b3b712fdf12ead8
pritibandewar-mindbowser <priti.b@mindbowser.com>
2021-11-06 18:58:32 +0000

    Resolved issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { groupArrayElements } from "./groupArrayElements.js";
 
 import "./App.css";
+
 const input = [1, 2, 3, 4, 5];
 const size = 3;
 const result = groupArrayElements(input, size);
@@ -8,8 +9,6 @@ const result = groupArrayElements(input, size);
 function App() {
   return (
     <div className="App">
-      <pre>Input :{JSON.stringify(input)} </pre>
-      <pre>size :{JSON.stringify(size)} </pre>
       <pre>Output :{JSON.stringify(result)}</pre>
     </div>
   );
